0ac7dbca4e75a7e7129acaeef3f3073e4a36e00e
Trevor Ingram <ingramtrevor10@gmail.com>
2023-09-13 22:14:37 -0700

    built .js and .css file to include class requirements

--- aboutMe.js
+++ aboutMe.js
@@ -1,5 +1,73 @@
-function username(){
-let name = prompt ("Before we get started, please tell me your name?")
-console.log (name)
-document.write ("Hello " + name + ", let's get started.")
+'use strict';
+
+let userName = prompt ("Before we get started please tell me your name?");
+//console.log (userName);
+alert("Hello " + userName + ", let's get started.");
+//console.log ("Before we get started please tell me your name? Hello " + userName + ", let's get started.")
+
+
+
+let questionOne = prompt('Is my name Trevor Ingram?').toLowerCase();
+
+if (questionOne ==='yes' || questionOne === 'y'){
+  //console.log('You are correct!');
+  alert('You are correct!');
+} else if (questionOne === 'no' || questionOne === 'n'){
+    //console.log('Wrong, try again.');
+    alert('Wrong, try again.');
+} else {
+    //console.log('Please answer with yes/no or y/n.');
+    alert('Please answer with yes/no or y/n.');
+}
+
+let questionTwo = prompt('Did I grow up in Oregon?').toLowerCase();
+
+if (questionTwo ==='yes' || questionTwo === 'y'){
+  //console.log('You are correct!');
+  alert('You are correct!');
+} else if (questionTwo === 'no' || questionTwo === 'n'){
+    //console.log('Wrong, try again.');
+    alert('Wrong, try again.');
+} else {
+    //console.log('Please answer with yes/no or y/n.');
+    alert('Please answer with yes/no or y/n.');
+}
+
+let questionThree = prompt('Did I ever live in Florida?').toLowerCase();
+
+if (questionThree ==='yes' || questionThree === 'y'){
+  //console.log('Wrong, try again.');
+  alert('Wrong, try again.');
+} else if (questionThree === 'no' || questionThree === 'n'){
+    //console.log('You are correct!');
+    alert('You are correct!');
+} else {
+    //console.log('Please answer with yes/no or y/n.');
+    alert('Please answer with yes/no or y/n.');
+}
+
+let questionFour = prompt('Did I attend college?').toLowerCase();
+
+if (questionFour ==='yes' || questionFour === 'y'){
+  //console.log('You are correct!');
+  alert('You are correct!');
+} else if (questionFour === 'no' || questionFour === 'n'){
+    //console.log('Wrong, try again.');
+    alert('Wrong, try again.');
+} else {
+    //console.log('Please answer with yes/no or y/n.');
+    alert('Please answer with yes/no or y/n.');
+}
+
+let questionFive = prompt('Do I still work at Cascades East Transit?').toLowerCase();
+
+if (questionFive ==='yes' || questionFive === 'y'){
+  //console.log('Wrong, try again.');
+  alert('Wrong, try again.');
+} else if (questionFive === 'no' || questionFive === 'n'){
+    //console.log('You are correct!');
+    alert('You are correct!');
+} else {
+    //console.log('Please answer with yes/no or y/n.');
+    alert('Please answer with yes/no or y/n.');
 }
